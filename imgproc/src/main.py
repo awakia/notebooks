b0c0d4fd41976b55e6848bd6343a1a1cde5368ad
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2019-05-20 16:24:36 +0900

    Add bitembed_edge_type method

--- imgproc/src/main.py
+++ imgproc/src/main.py
@@ -2,6 +2,7 @@ import glob
 import math
 import cv2
 import numpy as np
+import matplotlib.colors as colors
 
 import filters
 import visualize
@@ -21,7 +22,22 @@ def edge_type(gray):
   saturation = np.full(gray.shape, 255, dtype=np.uint8)
   hsv = np.dstack((hue, saturation, brightness))
   bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-  return bgr
+  visualize.img(bgr)
+  return hue, brightness
+
+def bitembed_edge_type(hue, brightness, ang=5, thresh=128):
+  lut = np.zeros((256, 1), dtype=np.uint16) # or 65536
+  for x in range(12):
+    for v in range(x*15-ang, x*15 +ang+1):
+      lut[v%180][0] = 1 << x # 1 + x
+  br = brightness.astype(np.uint16)
+  th, mask = cv2.threshold(br, 127, 65535, cv2.THRESH_BINARY)
+  # visualize.img(mask)
+  bits = cv2.LUT(hue, lut)
+  res = cv2.bitwise_and(bits, mask)
+  # print("res max,min,type", np.max(res), np.min(res), res.dtype)
+  visualize.img(res, 'hsv', colors.LogNorm())
+  return res
 
 def get_lines(gray):
   lsd = cv2.createLineSegmentDetector()
@@ -35,8 +51,9 @@ def proc(img):
   visualize.img(img, "gray")
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   # visualize.img(gray, "gray")
-  et = edge_type(gray)
-  visualize.img(et)
+  hue, brightness = edge_type(gray)
+  bitembed_edge_type(hue, brightness)
+
   # lines = get_lines(gray)
   # visualize.with_lines(img, lines)
 
